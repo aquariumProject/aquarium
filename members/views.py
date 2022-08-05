e4585db8d1f5e5039724626b9bd24cdc459c116a
alsdud019 <1926089@gmail.com>
2022-08-06 00:53:46 +0900

    회원가입

---
+++ members/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
+
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=RegisterSerializer
+
+
+# Create your views here.
